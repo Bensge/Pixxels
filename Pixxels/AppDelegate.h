4741e560b1597488d0e204f309059d93b87cf1e8
Bensge <bensge@me.com>
2013-03-06 17:59:28 +0100

    Initial Commit

---
+++ Pixxels/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Pixxels
+//
+//  Created by Benno Krauss on 06.03.13.
+//  Copyright (c) 2013 Benno Krauss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Pixxels/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Pixxels
+//
+//  Created by Benno Krauss on 06.03.13.
+//  Copyright (c) 2013 Benno Krauss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
